b6745d1a2fd895b7475ac1c623a04a4d4e777c75
Sebastien DIEUNIDOU <s.dieunidou@gmail.com>
2019-01-17 20:11:48 +0100

    add GameInfos api calls in SDK

---
+++ tests/Game.Infos.test.js
@@ -0,0 +1,27 @@
+/**
+ * mtxserv-node
+ * Node.JS SDK providing easy access to mTxServ's API
+ * @author Yann SEGET <dev@leafgard.fr>
+ * 
+ * JEST TEST
+ */
+
+require('dotenv').config()
+
+const mTxClient = require('../src/index').initialize({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  apiToken: process.env.API_KEY
+})
+
+test('Call resources() returns null & status 200', () => {
+  let req = mTxClient.Game.Infos.resources(process.env.SERVER_ID)
+  expect(typeof req.body).toBe('object')
+  expect(req.statusCode).toBe(200)
+})
+
+test('Call state() returns null & status 200', () => {
+  let req = mTxClient.Game.Infos.state(process.env.SERVER_ID)
+  expect(typeof req.body).toBe('object')
+  expect(req.statusCode).toBe(200)
+})
